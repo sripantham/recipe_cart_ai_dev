321738a8e5e73b97759e6c234de09724769a3f8c
sripantham <sripantham@gmail.com>
2024-11-04 16:01:50 -0800

    Update NextConfig

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: ['lucide-react','@google/generative-ai'],
+  output:'standalone',
 };
 
 export default nextConfig;
